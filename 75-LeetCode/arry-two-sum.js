aab6591465d752a0fcd98b12c527a4d088bddf64
changhyun yoo <chyoo0428@gmail.com>
2022-06-29 22:07:27 +0000

    completed twosum challenege

--- 75-LeetCode/arry-two-sum.js
+++ 75-LeetCode/arry-two-sum.js
@@ -1,9 +1,18 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+/* eslint-disable no-unused-vars */
 function twoSum(nums, target) {
-
+  const twoSum = function (nums, target) {
+    const index = [0, 0];
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] !== target) {
+          j++;
+        }
+        if (nums[i] + nums[j] === target) {
+          index[0] = i;
+          index[1] = j;
+        }
+      }
+    }
+    return index;
+  };
 }
-twoSum(null);
