37b88ec0897bfad690dd25eeedca4da2d702424e
changhyun yoo <chyoo0428@gmail.com>
2022-07-14 23:45:17 +0000

    completed maxprofit-review

--- 75-LeetCode/array-max-profit.js
+++ 75-LeetCode/array-max-profit.js
@@ -1,21 +1,12 @@
 /* eslint-disable no-unused-vars */
-/**
- * @param {number[]} prices
- * @return {number}
- */
 var maxProfit = function (prices) {
-  let mostProfit = 0;
+  let min = prices[0];
   let profit = 0;
-  let minimum = prices[0];
+  let mostProfit = 0;
   for (let i = 1; i < prices.length; i++) {
-    if (prices[i] < minimum) {
-      minimum = prices[i];
-      continue;
-    }
-    profit = prices[i] - minimum;
-    if (profit > mostProfit) {
-      mostProfit = profit;
-    }
+    min = prices[i] < min ? prices[i] : min;
+    profit = prices[i] - min;
+    mostProfit = profit >= mostProfit ? profit : mostProfit;
   }
   return mostProfit;
 };
