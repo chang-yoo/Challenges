20b27a1fb19e282a51b6eec01bd5dd4a21a2e92c
changhyun yoo <chyoo0428@gmail.com>
2022-07-18 00:42:19 +0000

    product except self

--- 75-LeetCode/array-productExceptSelf.js
+++ 75-LeetCode/array-productExceptSelf.js
@@ -4,14 +4,16 @@ var productExceptSelf = function (nums) {
   left[0] = 1;
   const right = Array(nums.length).fill(0);
   right[right.length - 1] = 1;
-  for (let i = 1; i < nums.length; i++) {
+  for (let i = 1; i < left.length; i++) {
     left[i] = nums[i - 1] * left[i - 1];
   }
   for (let i = right.length - 2; i >= 0; i--) {
     right[i] = nums[i + 1] * right[i + 1];
   }
   for (let i = 0; i < nums.length; i++) {
-    nums[i] = left[i] * right[i];
+    for (let i = 0; i < nums.length; i++) {
+      nums[i] = right[i] * left[i];
+    }
   }
   return nums;
 };
