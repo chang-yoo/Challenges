bb1352b26a62f49d98b5091aabc59ff0c9418191
changhyun yoo <chyoo0428@gmail.com>
2022-07-18 01:25:28 +0000

    maxproduct

--- 75-LeetCode/maximum-product-subarray.js
+++ 75-LeetCode/maximum-product-subarray.js
@@ -6,8 +6,9 @@ var maxProduct = function (nums) {
   for (let i = 1; i < nums.length; i++) {
     const current = nums[i];
     minValue[i] = Math.min(current, current * minValue[i - 1], current * maxValue[i - 1]);
-    maxValue[i] = Math.max(current, current * minValue[i - 1], current * maxValue[i - 1]);
-    max = maxValue[i] > max ? maxValue[i] : max;
+    maxValue[i] = Math.max(current, current * maxValue[i - 1], current * minValue[i - 1]);
+    max = maxValue[i] >= max ? maxValue[i] : max;
+
   }
   return max;
 };
