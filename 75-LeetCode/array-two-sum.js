2235da863bf7cd1cea2786f52526af24ff3f5a1d
LAPTOP-QMV1ALBU\CHANGHYUN YOO <chyoo0428@gmail.com>
2023-02-17 15:18:57 -0800

    found a way to get the answer without using loop

--- 75-LeetCode/array-two-sum.js
+++ 75-LeetCode/array-two-sum.js
@@ -3,9 +3,17 @@ const nums = [1,2,3,6,11]
 const target = 5
 
 const twoSum = function (nums, target) {
-  
-  for (let i = 0; i < nums.length; i++){
-    
-  }
+  let lastIdx = nums.length -1;
+  let idx = 0;
+  while (nums[idx]+nums[lastIdx] != target){
+    if (nums[idx]+nums[lastIdx] > target){
+      lastIdx--
+    } else {
+      idx++
+    }
+  } 
+  console.log(idx, lastIdx)
+  return [idx, lastIdx]
 };
+
 twoSum(nums, target)
