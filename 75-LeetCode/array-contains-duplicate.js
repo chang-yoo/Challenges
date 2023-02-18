7919f6c94c1ad2a703bbb6bbc17fa23049231da8
LAPTOP-QMV1ALBU\CHANGHYUN YOO <chyoo0428@gmail.com>
2023-02-17 16:46:18 -0800

    leetcode #2 done

--- 75-LeetCode/array-contains-duplicate.js
+++ 75-LeetCode/array-contains-duplicate.js
@@ -1,5 +1,12 @@
 /* eslint-disable no-unused-vars */
+var nums = [1,2,3,1]
 var containsDuplicate = function (nums) {
-  let memory = {};
-  console.log("HI")
+  nums = nums.sort();
+  for (var i = 0; i < nums.length; i++){
+    if (i+1 < nums.length-1 && nums[i] === nums[i+1]){
+      return true
+    }
+  }
+  return false;
 };
+console.log(containsDuplicate(nums))
