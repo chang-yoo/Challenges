b71d30f4c7d01808c210f564363a017680f16849
LAPTOP-QMV1ALBU\CHANGHYUN YOO <chyoo0428@gmail.com>
2023-02-21 09:16:39 -0800

    duplicate

--- 75-LeetCode/array-contains-duplicate.js
+++ 75-LeetCode/array-contains-duplicate.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-unused-vars */
 var nums = [1,2,3,1]
 var containsDuplicate = function (nums) {
-  nums = nums.sort();
-  for (var i = 0; i < nums.length; i++){
-    if (i+1 < nums.length-1 && nums[i] === nums[i+1]){
-      return true
-    }
+  nums.sort();
+  for (let i = 0 ; i < nums.length; i++){
+      if (i+1 < nums.length-1 && nums[i] === nums[i+1]){
+          return true
+      }
   }
   return false;
 };
-
-console.log(containsDuplicate(nums))
+containsDuplicate(nums)
