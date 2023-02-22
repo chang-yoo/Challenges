bf8c1a241a112ff8df7081954d2507130a1b7656
LAPTOP-QMV1ALBU\CHANGHYUN YOO <chyoo0428@gmail.com>
2023-02-22 09:59:19 -0800

    subarray done

--- 75-LeetCode/maximum-product-subarray.js
+++ 75-LeetCode/maximum-product-subarray.js
@@ -1,14 +1,15 @@
 // eslint-disable-next-line
 var maxProduct = function (nums) {
-  let max = nums[0];
-  const minValue = [nums[0]];
-  const maxValue = [nums[0]];
-  for (let i = 1; i < nums.length; i++) {
-    const current = nums[i];
-    minValue[i] = Math.min(current, current * minValue[i - 1], current * maxValue[i - 1]);
-    maxValue[i] = Math.max(current, current * maxValue[i - 1], current * minValue[i - 1]);
-    max = maxValue[i] >= max ? maxValue[i] : max;
+  let min = nums[0]
+  let max = nums[0]
+  let global = nums[0]
 
+  if (nums.length === 1) return nums[0]
+
+  for(let i = 1; i < nums.length; i++){
+    max = Math.max(max*nums[i], min*nums[i], nums[i])
+    min = Math.min(max*nums[i], min*nums[i], nums[i])
+    global = Math.max(global, max);
   }
-  return max;
+  return global
 };
